5f854e1557b5f769c4494ccc7da990f432e55f20
Velma32741 <95273840+Velma32741@users.noreply.github.com>
2022-02-13 06:19:15 +0700

    Add files via upload

---
+++ operator1.java
@@ -0,0 +1,12 @@
+package com.mycompany.operator1;
+
+/**
+ *
+ * @author FAKHRI
+ */
+class operator1{
+ public static void main(String[] args) {
+ double x = 92.98;
+ int nx = (int) Math.round(x);
+ }
+}
