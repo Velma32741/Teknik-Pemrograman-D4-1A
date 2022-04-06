8c62fc23ba87ece549d1de807dc4bb19847f6cf6
Velma32741 <95273840+Velma32741@users.noreply.github.com>
2022-04-06 18:35:54 +0700

    Add files via upload

---
+++ W7/src/main/java/w7/ATMMain.java
@@ -0,0 +1,13 @@
+package w7;
+
+/**
+ *
+ * @author FAKHRI
+ */
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
